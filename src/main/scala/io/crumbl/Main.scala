c959fbab40745a0663ee92281d8bb42b87d76fd6
Cyril Dever <cdever@edgewhere.fr>
2019-12-30 15:01:26 +0100

    wrap up in Main for executable

---
+++ src/main/scala/io/crumbl/Main.scala
@@ -0,0 +1,30 @@
+package io.crumbl
+
+import io.crumbl.client._
+import io.crumbl.utils.Logging
+
+import CrumblWorker._
+
+object Main extends App with Logging {
+  try {
+    // Prepare operation
+    val config = Config.init(args = args)
+    val mode: CrumblMode = {
+      if (config.c) CREATION
+      else if (config.x) EXTRACTION
+      else throw new Exception("invalid flag mode: use -c or -x")
+    }
+    if (!config.checks) throw new Exception("bad arguments: verify usage")
+
+    // Process operation
+    val worker = CrumblWorker(mode, config)
+    worker.process(false)
+  } catch {
+    case e: Exception =>
+      logger.severe(e.getMessage)
+      e.getStackTrace foreach println
+      println
+      println(Config.getUsage)
+      System.exit(1)
+  }
+}
